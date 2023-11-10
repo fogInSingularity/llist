8b6b3d657274ebf955a8e639ba9f3bc958c7a6ad
fogInSingularity <vladimirnaumov979@gmail.com>
2023-11-10 04:37:49 +0300

    added docs

--- listcode/include/llist.h
+++ listcode/include/llist.h
@@ -21,6 +21,7 @@ static const size_t kLListMinAlloc = 8;
 typedef int elem_t;
 typedef size_t index_t;
 
+/// @brief errors that list can throw
 enum class LListError {
   SUCCESS                    = 0,
   CTOR_CANT_ALLOC            = 1,
@@ -33,33 +34,138 @@ enum class LListError {
   POP_ON_EMPTY_LIST          = 8,
 };
 
+/// @brief list node
 struct LLNode {
-  elem_t elem;
-  index_t next;
-  index_t prev;
+  elem_t elem;  ///< node data
+  index_t next; ///< index of next node
+  index_t prev; ///< index of previous node
 };
 
+/// @brief double linked list
 struct LList {
  public:
+
+  /**
+   * @brief list constructor
+   *
+   * @param initCap (optional) init capacity
+   *
+   * @return list error
+  */
   LListError Ctor(size_t initCap = kLListMinAlloc);
+
+  /**
+   * @brief list destructor
+  */
   void Dtor();
+
+  /**
+   * @brief throw error to stdout
+   *
+   * @param error error to throw
+  */
   void ThrowError(LListError error);
+
+  /**
+   * @brief generates dump in .dot file format
+  */
   void DotDump();
 
+  /**
+   * @brief checks if the list has no nodes
+   *
+   * @return true if the list is empty, false otherwise
+  */
   bool IsEmpty();
+
+  /**
+   * @brief returns number of nodes in list
+   *
+   * @return number of nodes in list
+  */
   size_t Size();
+
+  /**
+   * @brief returns
+   *
+   * @return maximum number of nodes in list
+  */
   size_t Capacity();
 
+  /**
+   * @brief returns the index of the first node in the list
+   *
+   * @param indRet return index
+   *
+   * @return list error
+  */
   LListError Front(index_t* indRet);
+
+  /**
+   * @brief returns the index of the last node in the list
+   *
+   * @param indRet return index
+   *
+   * @return list error
+  */
   LListError Back(index_t* indRet);
 
+  /**
+   * @brief prepends the given node to the beginning of the list
+   *
+   * @param elem node to prepand
+   *
+   * @return list error
+  */
   LListError PushFront(elem_t elem);
+
+  /**
+   * @brief appends the given node to the end of the list
+   *
+   * @param elem node to append
+   *
+   * @return list error
+  */
   LListError PushBack(elem_t elem);
+
+  /**
+   * @brief removes the first node of the list
+   *
+   * @return list error
+  */
   LListError PopFront();
+
+  /**
+   * @brief removes the last node of the list
+   *
+   * @return list error
+  */
   LListError PopBack();
 
+  /**
+   * @brief insers element after given index
+   *
+   * @param ind index of the node before which the element will be inserted
+   * @param elem element of the node to insert
+   *
+   * @return list error
+  */
   LListError InsertAfter(index_t ind, elem_t elem);
+
+  /**
+   * @brief removes the node at the given index
+   *
+   * @param ind index of the node to remove
+   *
+   * @return list error
+  */
   LListError RemoveAt(index_t ind);
+
+  /**
+   * @brief linearize the list in the memory
+   *
+   * @return list error
+  */
   LListError Linearize();
  private:
   LLNode* list_;
